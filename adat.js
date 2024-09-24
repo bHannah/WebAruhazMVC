53068da5e766218b6664c9557ca84a7e7a42e931
vikierdos <csicsorka@gmail.com>
2024-09-24 18:57:26 +0200

    Adatokkal feltöltés

---
+++ adat.js
@@ -0,0 +1,299 @@
+export const DRESSLIST = [
+  {
+    name: "Amethyst Glow",
+    pic: "photos/amethyst_glow.png",
+    color: "purple",
+    price: 690
+  },
+  {
+    name: "Aurora Amour",
+    pic: "photos/aurora_amour.png",
+    color: "white",
+    price: 520
+  },
+  {
+    name: "Aurora Silk Mirage",
+    pic: "photos/aurora_silk_mirage.png",
+    color: "pink",
+    price: 690
+  },
+  {
+    name: "Azure Skies Corset",
+    pic: "photos/azure_skies_corset.png",
+    color: "blue",
+    price: 760
+  },
+  {
+    name: "Barbie Bombshell",
+    pic: "photos/barbie_bombshell.png",
+    color: "pink",
+    price: 470
+  },
+  {
+    name: "Blossom Breeze",
+    pic: "photos/blossom_breeze.png",
+    color: "purple",
+    price: 500
+  },
+  {
+    name: "Bluebell Bliss",
+    pic: "photos/bluebell_bliss.png",
+    color: "blue",
+    price: 620
+  },
+  {
+    name: "Celestial Charm",
+    pic: "photos/celestial_charm.png",
+    color: "white",
+    price: 700
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Crimson Velvet Ensemble",
+    pic: "photos/crimson_velvet_ensemble.png",
+    color: "red",
+    price: 699
+  },
+  {
+    name: "Crystal Cascade",
+    pic: "photos/crystal_cascade.png",
+    color: "white",
+    price: 1340
+  },
+  {
+    name: "Crystal Couture",
+    pic: "photos/crystal_couture.png",
+    color: "white",
+    price: 1700
+  },
+  {
+    name: "Dreamscape Symphony",
+    pic: "photos/dreamscape_symphony.png",
+    color: "pink",
+    price: 620
+  },
+  {
+    name: "Emerald Silk Sheath",
+    pic: "photos/emerald_silk_sheath.png",
+    color: "green",
+    price: 560
+  },
+  {
+    name: "Ethereal Prism Poise",
+    pic: "photos/ethereal_prism_poise.png",
+    color: "blue",
+    price: 540
+  },
+  {
+    name: "Frosty Dream",
+    pic: "photos/frosty_dream.png",
+    color: "purple",
+    price: 720
+  },
+  {
+    name: "Gilded Ivory Elegance",
+    pic: "photos/gilded_ivory_elegance.png",
+    color: "white, ivory",
+    price: 700
+  },
+  {
+    name: "Golden Glimmer",
+    pic: "photos/golden_glimmer.png",
+    color: "white",
+    price: 620
+  },
+  {
+    name: "Ivory Mist",
+    pic: "photos/ivory_mist.png",
+    color: "white",
+    price: 960
+  },
+  {
+    name: "Ivory Silk",
+    pic: "photos/ivory_silk.png",
+    color: "white",
+    price: 760
+  },
+  {
+    name: "Lagoon Jade Luster",
+    pic: "photos/lagoon_jade_luster.png",
+    color: "blue",
+    price: 620
+  },
+  {
+    name: "Lavender Blossom",
+    pic: "photos/lavender_blossom.png",
+    color: "purple, white",
+    price: 699
+  },
+  {
+    name: "Luminescent Diamond Serenade",
+    pic: "photos/luminescent_diamond_serenade.png",
+    color: "white",
+    price: 1340
+  },
+  {
+    name: "Luminescent Luxe",
+    pic: "photos/luminescent_luxe.png",
+    color: "white",
+    price: 1280
+  },
+  {
+    name: "Luxe Luster Sequin",
+    pic: "photos/luxe_luster_sequin.png",
+    color: "gold",
+    price: 470
+  },
+  {
+    name: "Mermaid's Melody",
+    pic: "photos/mermaids_melody.png",
+    color: "blue",
+    price: 620
+  },
+  {
+    name: "Midnight Ruffle Romance",
+    pic: "photos/midnight_ruffle_romance.png",
+    color: "black, white",
+    price: 999
+  },
+  {
+    name: "Midnight Shimmer Sonata",
+    pic: "photos/midnight_shimmer_sonata.png",
+    color: "black, gold",
+    price: 560
+  },
+  {
+    name: "Moonbeam Melody",
+    pic: "photos/moonbeam_melody.png",
+    color: "white",
+    price: 1340
+  },
+  {
+    name: "Moonlit Elegance",
+    pic: "photos/moonlit_elegance.png",
+    color: "white",
+    price: 720
+  },
+  {
+    name: "Moonlit Mist",
+    pic: "photos/moonlit_mist.png",
+    color: "white",
+    price: 820
+  },
+  {
+    name: "Noir Rose Romance",
+    pic: "photos/noir_rose_romance.png",
+    color: "black, white",
+    price: 620
+  },
+  {
+    name: "Olive Whisper",
+    pic: "photos/olive_whisper.png",
+    color: "green, black",
+    price: 620
+  },
+  {
+    name: "Orchid Opulence",
+    pic: "photos/orchid_opulence.png",
+    color: "purple",
+    price: 740
+  },
+  {
+    name: "Orion's Blue Twinkle",
+    pic: "photos/orions_blue_twinkle.png",
+    color: "blue, white",
+    price: 520
+  },
+  {
+    name: "Pearl Blossom",
+    pic: "photos/pearl_blossom.png",
+    color: "white",
+    price: 780
+  },
+  {
+    name: "Peony Princess",
+    pic: "photos/peony_princess.png",
+    color: "purple, pink, blue",
+    price: 670
+  },
+  {
+    name: "Petal Whispering",
+    pic: "photos/petal_whispering.png",
+    color: "white, pink",
+    price: 740
+  },
+  {
+    name: "Regal Diamond Radiance",
+    pic: "photos/regal_diamond_radiance.png",
+    color: "white",
+    price: 790
+  },
+  {
+    name: "Ruby Bloom Sheath",
+    pic: "photos/ruby_bloom_sheath.png",
+    color: "red",
+    price: 760
+  },
+  {
+    name: "Ruby Rendezvous",
+    pic: "photos/ruby_rendezvous.png",
+    color: "red",
+    price: 560
+  },
+  {
+    name: "Scarlet Dazzle",
+    pic: "photos/scarlet_dazzle.png",
+    color: "red, pink",
+    price: 590
+  },
+  {
+    name: "Scarlet Garden Serenade",
+    pic: "photos/scarlet_garden_serenade.png",
+    color: "red, white",
+    price: 720
+  },
+  {
+    name: "Scarlet Sangria",
+    pic: "photos/scarlet_sangria.png",
+    color: "red, brown",
+    price: 780
+  },
+  {
+    name: "Seraphic Diamond",
+    pic: "photos/seraphic_diamond.png",
+    color: "white",
+    price: 1400
+  },
+  {
+    name: "Soft Petal Melody",
+    pic: "photos/soft_petal_melody.png",
+    color: "white, pink, purple, blue",
+    price: 680
+  },
+  {
+    name: "Strawberry Sparkle",
+    pic: "photos/strawberry_sparkle.png",
+    color: "red, pink, white, silver",
+    price: 740
+  },
+  {
+    name: "Sweet Serenity",
+    pic: "photos/sweet_serenity.png",
+    color: "pink",
+    price: 720
+  },
+  {
+    name: "Twilight Diamond Drape",
+    pic: "photos/twilight_diamond_drape.png",
+    color: "white, ivory, gold",
+    price: 940
+  }
+  
+
+
+];
