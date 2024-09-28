ac3544ad4e5515c70ac4dc456da6a563406d6efd
vikierdos <csicsorka@gmail.com>
2024-09-28 19:35:38 +0200

    admin oldal elkezése, css, bootstrap

--- View/Kartya.js
+++ View/Kartya.js
@@ -9,9 +9,12 @@ export default class Kartya{
     //tagfüggvények
     kartyaMegjelenit(){
         this.szuloElem.append(`
-            <div class="card col-sm kartya" id="box">
-            <img src="${this.#obj.pic}" alt="${this.#obj.name}">
-            <p class="card-text">${this.#obj.color}</p>
+            <div class="col-lg-4 col-sm-5 p-2">
+            <div class="kartya">
+            <img src="${this.#obj.pic}" alt="${this.#obj.name}" class="w-100">
+            <p>${this.#obj.name}</p>
+            <button>Kosárba</button>
+            </div>
             </div>`)
     }
 }
